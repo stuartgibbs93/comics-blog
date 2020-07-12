7c98ac4e756bad90fc33b1b49169792aba8c824a
stuartgibbs93 <stuartgibbs93@gmail.com>
2020-07-12 13:58:27 -0400

    basic html layout setup

--- _homepage.php
+++ _homepage.php
@@ -8,7 +8,7 @@ get_header();
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
             <div class="jumbotron jumbotron-fluid">
-                <div class="container">
+                <div class="container-fluid">
                     <!-- <h1 class="display-4"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></h1> -->
                         <div class="site-branding mx-auto text-center col-4 mt-5">
 				            <img src="http://localhost:8888/marvelcomicsfandom/wp-content/uploads/2020/07/placeholder_logo.png">
@@ -16,6 +16,44 @@ get_header();
                         <!-- <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> -->
                 </div>
             </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6 col-12 d-flex flex-column align-items-center justify-content-center px-5" id="about">
+                        <h2 class="text-center">About</h2>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi eros, volutpat in mi id, interdum pellentesque odio. Praesent ut sollicitudin augue. Sed vitae lacus mi. Sed semper metus at lacus malesuada faucibus. Phasellus fermentum consequat euismod. In ut elit dui. Phasellus ac vehicula mi, vel sollicitudin ex. Phasellus egestas blandit nulla id efficitur. Praesent vehicula euismod est in elementum. Donec posuere neque faucibus quam congue eleifend vel non dolor. Integer sit amet orci et eros tristique maximus. Suspendisse potenti.</p>
+
+                    </div>
+                    <div class="col-lg-6 col-12" id="comics-background">
+                        <p>&nbsp;</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid d-flex flex-column align-items-center justify-content-center" id="reading-orders">
+                <div class="row">
+                    <div class="col-12 py-4 px-5 mt-5">
+                    <h2 class="text-center">Reading Orders</h2>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi eros, volutpat in mi id, interdum pellentesque odio. Praesent ut sollicitudin augue. Sed vitae lacus mi. Sed semper metus at lacus malesuada faucibus. Phasellus fermentum consequat euismod. In ut elit dui. Phasellus ac vehicula mi, vel sollicitudin ex. Phasellus egestas blandit nulla id efficitur. Praesent vehicula euismod est in elementum. Donec posuere neque faucibus quam congue eleifend vel non dolor. Integer sit amet orci et eros tristique maximus. Suspendisse potenti.</p>
+                    </div>
+                </div>
+            </div>
+           
+            <div class="container-fluid d-flex flex-column align-items-center justify-content-center" id="recommendations">
+                <div class="row">
+                    <div class="col-12 py-4 px-5 mt-5">
+                    <h2 class="text-center">Recommendations</h2>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi eros, volutpat in mi id, interdum pellentesque odio. Praesent ut sollicitudin augue. Sed vitae lacus mi. Sed semper metus at lacus malesuada faucibus. Phasellus fermentum consequat euismod. In ut elit dui. Phasellus ac vehicula mi, vel sollicitudin ex. Phasellus egestas blandit nulla id efficitur. Praesent vehicula euismod est in elementum. Donec posuere neque faucibus quam congue eleifend vel non dolor. Integer sit amet orci et eros tristique maximus. Suspendisse potenti.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid d-flex flex-column align-items-center justify-content-center" id="collection">
+                <div class="row">
+                    <div class="col-12 py-4 px-5 mt-5">
+                    <h2 class="text-center">Collection</h2>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi eros, volutpat in mi id, interdum pellentesque odio. Praesent ut sollicitudin augue. Sed vitae lacus mi. Sed semper metus at lacus malesuada faucibus. Phasellus fermentum consequat euismod. In ut elit dui. Phasellus ac vehicula mi, vel sollicitudin ex. Phasellus egestas blandit nulla id efficitur. Praesent vehicula euismod est in elementum. Donec posuere neque faucibus quam congue eleifend vel non dolor. Integer sit amet orci et eros tristique maximus. Suspendisse potenti.</p>
+                    </div>
+                </div>
+            </div>
+
                     
 
         </main><!-- #main -->
--- footer.php
+++ footer.php
@@ -12,18 +12,8 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'comics-blog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'comics-blog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'comics-blog' ), 'comics-blog', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+		<div class="site-info p-2">
+			<p class="m-0">2020 &copy; MarvelComicsFandom</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
